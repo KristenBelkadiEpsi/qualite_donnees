bcfaae50c4073b93b315e725052d3dc7a0ec0b5e
debian-kristen <debian-kristen@serveur-mail.kristen>
2023-10-19 11:56:22 +0200

    affichage circuits

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,15 +9,28 @@
       <l-marker :lat-lng="(center as [number, number])">
         <l-tooltip>EPSI</l-tooltip></l-marker
       >
+      <l-geo-json
+        v-for="(circuit, index) in circuits"
+        :geojson="circuit"
+        :key="index"
+      >
+      </l-geo-json>
     </LMap>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { LMap, LTileLayer, LMarker, LTooltip } from "@vue-leaflet/vue-leaflet";
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LTooltip,
+  LGeoJson,
+} from "@vue-leaflet/vue-leaflet";
 import { PointExpression } from "leaflet";
 import "leaflet/dist/leaflet.css";
+import axios from "axios";
 export default defineComponent({
   name: "HomeView",
   components: {
@@ -25,15 +38,29 @@ export default defineComponent({
     LTileLayer,
     LMarker,
     LTooltip,
+    LGeoJson,
   },
+
   data() {
     return {
       zoom: 13,
       center: [47.20624365988347, -1.5393715932931804],
+      circuits: [],
     };
   },
-  mounted() {
-    console.log("chargé");
+  methods: {
+    async getCircuits() {
+      axios
+        .get(
+          "https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_tan-circuits/records?limit=-1"
+        )
+        .then((reponse: any) => {
+          this.circuits = reponse.data.results.map((e: any) => e.shape);
+        });
+    },
+  },
+  async mounted() {
+    await this.getCircuits();
   },
 });
 </script>
