71e366d42521aca520f49af609a58df61af6a74d
benoitAD <benoit.augerdubois@gmail.com>
2023-10-20 10:33:49 +0200

    fix: marker resize

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="height: 1080px; width: 1920px">
-    <LMap :zoom="zoom" :center="(center as PointExpression)" ref="map">
+    <LMap :zoom="zoom" :center="(center as PointExpression)" ref="map" @update:zoom="zoomUpdate"	>
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
@@ -26,6 +26,10 @@
         :key="index"
         :lat-lng="([arret.stop_coordinates.lat, arret.stop_coordinates.lon] as [number,number])"
       >
+      <l-icon
+          :icon-size="icon.iconSize"
+          icon-url="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-512.png"
+        />
         <l-tooltip>{{ arret.stop_name }}</l-tooltip>
       </l-marker>
     </LMap>
@@ -39,7 +43,8 @@ import {
   LTileLayer,
   LMarker,
   LTooltip,
-  LGeoJson
+  LGeoJson,
+  LIcon,
 } from "@vue-leaflet/vue-leaflet";
 import { PointExpression } from "leaflet";
 import "leaflet/dist/leaflet.css";
@@ -52,7 +57,8 @@ export default defineComponent({
     LTileLayer,
     LMarker,
     LTooltip,
-    LGeoJson
+    LGeoJson,
+    LIcon,
   },
 
   data() {
@@ -61,11 +67,27 @@ export default defineComponent({
       center: [47.20624365988347, -1.5393715932931804],
       circuits: <any[]>[],
       nombreArrets: 0,
-      arrets: <any[]>[]
+      arrets: <any[]>[],
+      icon: {
+        iconUrl: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-512.png",
+        iconSize: [10, 10] as [number, number],
+      },
     };
 
   },
   methods: {
+    zoomUpdate(zoom: number)  {
+      const baseIconSize = 4; // Taille de l'icône à un zoom de 0
+
+
+      const adjustedIconSize = baseIconSize + (zoom * 2); // Ajuster la taille en fonction du zoom
+
+
+      this.icon = {
+        iconUrl: "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-512.png",
+        iconSize: [adjustedIconSize, adjustedIconSize] as [number, number],
+      }
+    },
     async getCircuits() {
       axios
         .get(
@@ -88,7 +110,7 @@ export default defineComponent({
         .then((result) => {
           this.nombreArrets = result.data.total_count;
         });
-      for (let i = 0; i <= this.nombreArrets; i += 100) {
+        for (let i = 0; i <= this.nombreArrets; i += 100) { 
         await axios
           .get(
             `https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_tan-arrets/records?offset=${i}&limit=100`
@@ -97,7 +119,6 @@ export default defineComponent({
             let tab: any[] = reponse.data.results;
             tab.forEach(e => this.arrets.push(e));
           });
-
       }
 
     }
